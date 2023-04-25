d9abbb1b002bbe11e733bf15abe5741a5ed2b526
Wenkai Liang <wenkail@princeton.edu>
2023-04-25 14:24:35 +0800

    Update ode.py

--- ode.py
+++ ode.py
@@ -7,3 +7,12 @@ b = 0.25
 c = 5.0
 y0 = [np.pi - 0.1, 0.0]
 t = np.linspace(0, 10, 101)
+from scipy.integrate import odeint
+sol = odeint(pend, y0, t, args=(b, c))
+import matplotlib.pyplot as plt
+plt.plot(t, sol[:, 0], 'b', label='theta(t)')
+plt.plot(t, sol[:, 1], 'g', label='omega(t)')
+plt.legend(loc='best')
+plt.xlabel('t')
+plt.grid()
+plt.show()
