396088d81f1473e337d5539f1ac3d38aa1aea060
Wenkai Liang <wenkail@princeton.edu>
2023-04-26 13:57:24 +0800

    Update ode.py

--- ode.py
+++ ode.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
 def pend(y, t, b, c):
     theta, omega = y
     dydt = [omega, -b*omega - c*np.sin(theta)]
@@ -7,9 +10,9 @@ b = 0.25
 c = 5.0
 y0 = [np.pi - 0.1, 0.0]
 t = np.linspace(0, 10, 101)
-from scipy.integrate import odeint
+
 sol = odeint(pend, y0, t, args=(b, c))
-import matplotlib.pyplot as plt
+
 plt.plot(t, sol[:, 0], 'b', label='theta(t)')
 plt.plot(t, sol[:, 1], 'g', label='omega(t)')
 plt.legend(loc='best')
