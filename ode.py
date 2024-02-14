dcfe45125388403dfa6b13049156ebafdf6e92be
Wenkai Liang <wkliang.pku@gmail.com>
2024-02-14 09:13:11 +0800

    Update ode.py

--- ode.py
+++ ode.py
@@ -1,20 +1,21 @@
 import numpy as np
-#from scipy.integrate import odeint
-from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
 # need to change ode
-def pend(y, t, b, c):
+# here!
+def ode(y, t, b, c):
     theta, omega = y
     dydt = [omega, -b*omega - c*np.sin(theta)]
     return dydt
+    
 # define
 b = 0.25
 c = 5.0
 y0 = [np.pi - 0.1, 0.0]
 t = np.linspace(0, 10, 101)
 
-sol = odeint(pend, y0, t, args=(b, c))
+sol = odeint(ode, y0, t, args=(b, c))
 
 plt.plot(t, sol[:, 0], 'b', label='theta(t)')
 plt.plot(t, sol[:, 1], 'g', label='omega(t)')
